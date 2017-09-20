04b2773872e6564658df5d142d931fcc1b04a11d
ArjunThakurDev <arjunthakur@live.com>
2017-09-20 22:52:48 +0000

    Initial Setup for Bot
    Initial Setup for Bot

---
+++ index.js
@@ -0,0 +1,22 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: "8fff4c9b-acb4-46f5-8727-f01d908b952c", //process.env.MICROSOFT_APP_ID,
+    appPassword: "4yb1Mhemjc6Yz7M4z0Mvp9S" //process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
